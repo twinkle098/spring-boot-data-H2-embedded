c7486f0fce9ce1cae9774641e01aa6f7fca38319
twinkle098 <76721356+twinkle098@users.noreply.github.com>
2022-01-20 16:57:19 +0530

    Create jenkins_mvn.groovy

---
+++ vars/jenkins_mvn.groovy
@@ -0,0 +1,67 @@
+defcall(StringrepoUrl) {
+
+pipeline{
+
+agent any
+
+stages{
+
+stage("Checkout Code") {
+
+steps {
+
+git branch: 'master',
+
+url: "${repoUrl}"
+
+}
+
+}
+
+stage('Compile Stage'){
+
+steps{
+
+withMaven(maven: 'Maven3'){
+
+bat 'mvn clean compile'
+
+}
+
+}
+
+}
+
+stage('Test Stage'){
+
+steps{
+
+withMaven(maven: 'Maven3'){
+
+bat 'mvn test'
+
+}
+
+}
+
+}
+
+stage('Build Stage'){
+
+steps{
+
+withMaven(maven: 'Maven3'){
+
+bat 'mvn install'
+
+}
+
+}
+
+}
+
+}
+
+}
+
+}
