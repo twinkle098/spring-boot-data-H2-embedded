adae9975e7d390061ad85f007edfa45e515235ce
twinkle098 <76721356+twinkle098@users.noreply.github.com>
2022-01-20 17:50:57 +0530

    Update jenkins_mvn.groovy

--- vars/jenkins_mvn.groovy
+++ vars/jenkins_mvn.groovy
@@ -1,67 +1,34 @@
-def call(StringrepoUrl) {
-
+def call(String repoUrl) {
 pipeline{
-     
 agent any
-     
 stages{
-
 stage("Checkout Code") {
-
 steps {
-
 git branch: 'master',
-
 url: "${repoUrl}"
-
 }
-
 }
-
 stage('Compile Stage'){
-
 steps{
-
 withMaven(maven: 'Maven3'){
-
 bat 'mvn clean compile'
-
 }
-
 }
-
 }
-
 stage('Test Stage'){
-
 steps{
-
 withMaven(maven: 'Maven3'){
-
 bat 'mvn test'
-
 }
-
 }
-
 }
-
 stage('Build Stage'){
-
 steps{
-
 withMaven(maven: 'Maven3'){
-
 bat 'mvn install'
-
 }
-
 }
-
 }
-
 }
-
 }
-
 }
